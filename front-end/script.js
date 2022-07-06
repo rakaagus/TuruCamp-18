12b3fd2bf093a22b01f9d902909880fe839d49b5
fajar <fajar23.septianto@gmail.com>
2022-07-06 09:30:41 +0700

    ini front endnya.. integrasinya gw serahin ke lu pada

---
+++ front-end/script.js
@@ -0,0 +1,11 @@
+const active = document.querySelector('.beranda');
+const active1 = document.querySelector('.daftar');
+const active2 = document.querySelector('.rating');
+const active3 = document.querySelector('.regis');
+
+active.addEventListener('click',function(){
+    active.classList.toggle('active')
+})
+active2.addEventListener('click',function(){
+    active2.classList.toggle('active')
+})
